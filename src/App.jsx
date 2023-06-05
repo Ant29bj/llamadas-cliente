c5af3c1d2a2feb58513571edd45bea84bcf16891
Ant29bj <bojorquez29an@gmail.com>
2023-06-04 19:43:09 -0700

    Ahi ta

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,50 @@ function App() {
   const [fechaInicio, setFechaInicio] = useState("");
   const [fechaFinal, setFechaFinal] = useState("");
 
+  const [src, setScr] = useState("");
+  const [dst, setDst] = useState("");
+
+  const [sucursales, setSucursales] = useState([]);
+  const [selectedSucursal, setSelectedSucursal] = useState("");
+
   const pageSize = 300;
   const apiUrl = "http://localhost:8080/llamadas";
 
   const fetchData = async () => {
     const url = `${apiUrl}?page=1&page_size=${pageSize}`;
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setCdrs(data);
+      });
+  };
+
+  const fetchSucursales = async () => {
+    const url = `${apiUrl}/clid`;
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setSucursales(data);
+      });
+  };
+
+  const getLlamadasSrc = async (e) => {
+    const url = `${apiUrl}/src?page=1&page_size=${pageSize}&src=${e}`;
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setCdrs(data);
+      });
+  };
+
+  const getLlamadasDst = async (e) => {
+    const url = `${apiUrl}/dst?page=1&page_size=${pageSize}&dst=${e}`;
     fetch(url)
       .then((response) => {
         return response.json();
@@ -46,6 +85,19 @@ function App() {
       setCdrs(response.data);
     } catch (error) {
       console.error(error);
+    }
+  };
+
+  const getLlamadasPorSucursal = async (e) => {
+    const url = `${apiUrl}/clid/filtro`;
+    console.log(e);
+    try {
+      const response = await axios.post(url, {
+        clid: e,
+      });
+      setCdrs(response.data);
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -71,11 +123,18 @@ function App() {
   const handleSelectFilter = (e) => {
     const value = e.target.value;
     setFiltro(value);
-
     if (value === "disposition") {
       setFilterOpt(["ANSWERED", "BUSY", "FAILED"]);
+      setFiltro("disposition");
+    } else if (value === "src") {
+      setFiltro("src");
+    } else if (value === "dst") {
+      setFiltro("dst");
     } else if (value === "fecha") {
-      console.log("hola");
+      setFiltro("fecha");
+    } else if (value === "sucursal") {
+      setFiltro("sucursal");
+      fetchSucursales();
     } else {
       setFilterOpt("");
     }
@@ -98,6 +157,56 @@ function App() {
   });
   return (
     <div className="wrapper">
+      {filtro === "dst" ? (
+        <div className="input-group input-group-sm mb-3">
+          <span className="input-group-text" id="inputGroup-sizing-sm">
+            Destino
+          </span>
+          <input
+            type="text"
+            className="form-control"
+            aria-describedby="inputGroup-sizing-sm"
+            value={dst}
+            onChange={(e) => {
+              setDst(e.target.value);
+            }}
+          />
+          <button
+            type="button"
+            className="btn"
+            onClick={() => {
+              getLlamadasDst(dst);
+            }}
+          >
+            Buscar
+          </button>
+        </div>
+      ) : null}
+      {filtro === "src" ? (
+        <div className="input-group input-group-sm mb-3">
+          <span className="input-group-text" id="inputGroup-sizing-sm">
+            Fuente
+          </span>
+          <input
+            type="text"
+            className="form-control"
+            aria-describedby="inputGroup-sizing-sm"
+            value={src}
+            onChange={(e) => {
+              setScr(e.target.value);
+            }}
+          />
+          <button
+            type="button"
+            className="btn"
+            onClick={() => {
+              getLlamadasSrc(src);
+            }}
+          >
+            Buscar
+          </button>
+        </div>
+      ) : null}
       {filtro === "fecha" ? (
         <div className="date-picker-container">
           <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -135,9 +244,46 @@ function App() {
         <option value="disposition">Estado</option>
         <option value="fecha">Fecha</option>
         <option value="src">Fuente</option>
-        <option value="destino">Destino</option>
+        <option value="dst">Destino</option>
+        <option value="sucursal">Sucursal</option>
       </select>
-      {filterOpt.length > 0 && (
+
+      {filtro === "sucursal" ? (
+        <div className="date-picker-container">
+          <select
+            className="form-select"
+            aria-label="Select sucursal"
+            value={selectedSucursal}
+            onChange={(e) => {
+              setSelectedSucursal(e.target.value);
+            }}
+          >
+            <option value="">Default</option>
+            {sucursales.map((option, index) => (
+              <option key={index} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+
+          <button
+            type="button"
+            className="btn"
+            onClick={() => {
+              const formatSucursal = (value) => {
+                const transformedValue = `"${value.replace(/"/g, '\\"')}"`;
+                return transformedValue;
+              };
+
+              const data = formatSucursal(selectedSucursal);
+              getLlamadasPorSucursal(selectedSucursal);
+            }}
+          >
+            Buscar
+          </button>
+        </div>
+      ) : null}
+      {filtro === "disposition" ? (
         <select
           className="form-select"
           value={dispositionSelected}
@@ -150,7 +296,7 @@ function App() {
             </option>
           ))}
         </select>
-      )}
+      ) : null}
       <div className="tabla-container">
         <Tabla llamadas={cdrs.records} />
         {/*
